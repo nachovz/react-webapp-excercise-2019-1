e50836a1ef3572fd601f04b8a8dd50f542ddb8d7
yusniel <eyusniel@gmail.com>
2019-02-07 01:27:40 +0000

    first commit

--- src/js/views/checkout.jsx
+++ src/js/views/checkout.jsx
@@ -2,6 +2,27 @@ import React from "react";
 
 export class Checkout extends React.Component {
 	render() {
-		return <h1>CHECKOUT!</h1>;
+		return (
+			<div>
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<img
+							src="/docs/4.2/assets/brand/bootstrap-solid.svg"
+							width="30"
+							height="30"
+							alt=""
+						/>
+						<h1 className="display-4">Checkout form</h1>
+						<p className="lead">
+							Below is an example form built entirely with
+							Bootstraps form controls. Each required form group
+							has a validation state that can be triggered by
+							attempting to sub0mit the form without completing
+							it.
+						</p>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
