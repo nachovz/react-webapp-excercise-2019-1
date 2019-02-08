62e46622dce883f3e4c7ba160fed9d35269517ed
Camilo <camilo.coo250@gmail.com>
2019-02-08 20:01:45 +0000

    store view done

--- src/js/views/storeView.jsx
+++ src/js/views/storeView.jsx
@@ -4,25 +4,102 @@ export class StoreView extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="jumbotron">
-					<h1 className="display-4">Hello, world!</h1>
-					<p className="lead">
+				<div className="jumbotron mb-0">
+					<h1 className="display-4 text-center">Hello, world!</h1>
+					<p className="lead text-center">
 						This is a simple hero unit, a simple jumbotron-style
 						component for calling extra attention to featured
 						content or information.
 					</p>
-					<p>
-						It uses utility classes for typography and spacing to
-						space content out within the larger container.
-					</p>
-					<p className="lead">
+
+					<p className="lead text-center">
 						<a
-							className="btn btn-primary btn-lg"
+							className="btn btn-light btn-lg"
 							href="#"
 							role="button">
 							Learn more
 						</a>
 					</p>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-6 bg-dark">
+						<h2 className="display-5 text-light text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-light text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-light shadow-sm mx-auto black" />
+					</div>
+					<div className="col-12 col-md-6 bg-light">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-dark shadow-sm mx-auto black" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-6 bg-light">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-dark shadow-sm mx-auto black" />
+					</div>
+					<div className="col-12 col-md-6 bg-primary">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-light shadow-sm mx-auto black" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-6 bg-light">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-light shadow-sm mx-auto black" />
+					</div>
+					<div className="col-12 col-md-6 bg-light">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-dark shadow-sm mx-auto black" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-6 bg-light">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-dark shadow-sm mx-auto black" />
+					</div>
+					<div className="col-12 col-md-6 bg-light">
+						<h2 className="display-5 text-dark text-center">
+							First row header
+						</h2>
+						<p className="display-5 text-dark text-center">
+							And an even wittier subheading.
+						</p>
+						<div className="bg-light shadow-sm mx-auto black" />
+					</div>
 				</div>
 			</div>
 		);
