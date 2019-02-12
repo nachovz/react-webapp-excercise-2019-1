675cf7cdd607ae160a513488703fb9df707dcaa2
Gloria Andino <andinoga@gmail.com>
2019-02-12 01:31:16 +0000

    added posts

--- src/js/component/blog.jsx
+++ src/js/component/blog.jsx
@@ -49,5 +49,39 @@ export class Blog extends React.Component {
 		</div>
 	</div>
 </div>;
+
+			<main role="main" class="container">
+				<div class="row">
+					<div class="col-md-8 blog-main">
+						<h3 class="pb-4 mb-4 font-italic border-bottom">
+							From the Firehouse
+							</h3>
+						<div class="blog-post">
+							<h2 class="blog-post-title">Sample blog post </h2>
+							<p class="blog-post-meta">
+								"January 1, 2014 by "
+								<a href="#">Mark</a>
+							</p>
+							<p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, images, and code are all supported.
+							</p>
+							<hr></hr>
+							<p>
+								"Cum sociis natoque penatibus et magnis"
+								<a href="#">dis parturient montes</a>
+								", nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum."
+								</p>
+								<blockquote>
+									<p>
+										"Curabitur blandit tempus porttitor."
+										<strong>Nullam quis risus eget urna mollis</strong>
+										" ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit."
+										</p>
+								</blockquote>
+							</div>
+						</div>
+					</div>
+				</main>;
+			
+					
 	}
 }
