7ea4b75097ef7de10b035909a233193908547b47
Camilo Contreras <43814860+Camilocoo@users.noreply.github.com>
2019-02-13 19:05:14 -0500

    store view  (#5)
    * first commit
    
    * jumbotron
    
    * store view done
    
    * first update
    
    * cards component created plus store view fixed
    
    * styles added
    
    * store view fixed

--- src/js/views/storeView.jsx
+++ src/js/views/storeView.jsx
@@ -1,104 +1,90 @@
 import React from "react";
+import Cards from "../component/cards.jsx";
+import Navbar from "../component/navbar.jsx";
+import Footer from "../component/footer.jsx";
 
 export class StoreView extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="jumbotron mb-0">
-					<h1 className="display-4 text-center">Hello, world!</h1>
-					<p className="lead text-center">
-						This is a simple hero unit, a simple jumbotron-style
-						component for calling extra attention to featured
-						content or information.
-					</p>
+				<div className="container-fluid">
+					<div className="jumbotron mb-0">
+						<h1 className="display-4 text-center">Hello, world!</h1>
+						<p className="lead text-center">
+							This is a simple hero unit, a simple jumbotron-style
+							component for calling extra attention to featured
+							content or information.
+						</p>
 
-					<p className="lead text-center">
-						<a
-							className="btn btn-light btn-lg"
-							href="#"
-							role="button">
-							Learn more
-						</a>
-					</p>
-				</div>
-				<div className="row">
-					<div className="col-12 col-md-6 bg-dark">
-						<h2 className="display-5 text-light text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-light text-center">
-							And an even wittier subheading.
+						<p className="lead text-center">
+							<a
+								className="btn btn-light btn-lg"
+								href="#"
+								role="button">
+								Learn more
+							</a>
 						</p>
-						<div className="bg-light shadow-sm mx-auto black" />
-					</div>
-					<div className="col-12 col-md-6 bg-light">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-dark shadow-sm mx-auto black" />
 					</div>
 				</div>
-				<div className="row">
-					<div className="col-12 col-md-6 bg-light">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-dark shadow-sm mx-auto black" />
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-dark"
+								shadowColor="bg-light"
+								textColor="text-white"
+							/>
+						</div>
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-light"
+								shadowColor="bg-dark"
+								textColor="text-black"
+							/>
+						</div>
 					</div>
-					<div className="col-12 col-md-6 bg-primary">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-light shadow-sm mx-auto black" />
+					<div className="row">
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-light"
+								shadowColor="bg-dark"
+							/>
+						</div>
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-primary"
+								shadowColor="bg-white"
+								textColor="text-white"
+							/>
+						</div>
 					</div>
-				</div>
-				<div className="row">
-					<div className="col-12 col-md-6 bg-light">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-light shadow-sm mx-auto black" />
+					<div className="row">
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-light"
+								shadowColor="bg-white"
+							/>
+						</div>
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-light"
+								shadowColor="bg-white"
+							/>
+						</div>
 					</div>
-					<div className="col-12 col-md-6 bg-light">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-dark shadow-sm mx-auto black" />
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-12 col-md-6 bg-light">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-dark shadow-sm mx-auto black" />
-					</div>
-					<div className="col-12 col-md-6 bg-light">
-						<h2 className="display-5 text-dark text-center">
-							First row header
-						</h2>
-						<p className="display-5 text-dark text-center">
-							And an even wittier subheading.
-						</p>
-						<div className="bg-light shadow-sm mx-auto black" />
+					<div className="row">
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-light"
+								shadowColor="bg-white"
+							/>
+						</div>
+						<div className="col-12 col-md-6 py-0 py-md-2">
+							<Cards
+								backgroundColor="bg-light"
+								shadowColor="bg-white"
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
