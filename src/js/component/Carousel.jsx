5ad61c2334dea94b515e63ef73d19a3796b6f83b
Azul Botaya <admbotaya@gmail.com>
2019-02-15 23:14:03 +0000

    carousel

--- src/js/component/Carousel.jsx
+++ src/js/component/Carousel.jsx
@@ -1,72 +1,55 @@
 import React from "react";
+import { ImageSlide } from "./ImageSlide.jsx";
+import { Arrow } from "./Arrow.jsx";
 
 export class Carousel extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentImageIndex: 0
+		};
+	}
+
+	previousSlide = () => {
+		const lastIndex = Image.length - 1;
+		const { currentImageIndex } = this.state;
+		const shouldResetIndex = currentImageIndex === 0;
+		const index = shouldResetIndex ? lastIndex : currentImageIndex - 1;
+		this.setState({
+			currentImageIndex: index
+		});
+	};
+
+	nextSlide = () => {
+		const lastIndex = Image.length - 1;
+		const { currentImageIndex } = this.state;
+		const shouldResetIndex = currentImageIndex === lastIndex;
+		const index = shouldResetIndex ? 0 : currentImageIndex + 1;
+		this.setState({
+			currentImageIndex: index
+		});
+	};
+
 	render() {
 		return (
-			<div
-				id="carouselExampleIndicators"
-				className="carousel slide"
-				data-ride="carousel">
-				<ol className="carousel-indicators">
-					<li
-						data-target="#carouselExampleIndicators"
-						data-slide-to="0"
-						className="active"
-					/>
-					<li
-						data-target="#carouselExampleIndicators"
-						data-slide-to="1"
-					/>
-					<li
-						data-target="#carouselExampleIndicators"
-						data-slide-to="2"
-					/>
-				</ol>
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img
-							className="d-block w-100"
-							src="http://placehold.it/250x100"
-							alt="First slide"
-						/>
-					</div>
-					<div className="carousel-item">
-						<img
-							className="d-block w-100"
-							src="http://placehold.it/250x100"
-							alt="Second slide"
-						/>
-					</div>
-					<div className="carousel-item">
-						<img
-							className="d-block w-100"
-							src="http://placehold.it/250x100"
-							alt="Third slide"
-						/>
-					</div>
-				</div>
-				<a
-					className="carousel-control-prev"
-					href="#carouselExampleIndicators"
-					role="button"
-					data-slide="prev">
-					<span
-						className="carousel-control-prev-icon"
-						aria-hidden="true"
-					/>
-					<span className="sr-only">Previous</span>
-				</a>
-				<a
-					className="carousel-control-next"
-					href="#carouselExampleIndicators"
-					role="button"
-					data-slide="next">
-					<span
-						className="carousel-control-next-icon"
-						aria-hidden="true"
-					/>
-					<span className="sr-only">Next</span>
-				</a>
+			<div className="carousel">
+				<Arrow
+					direction="left"
+					clickFunction={this.previousSlide}
+					glyph="&#9664;"
+				/>
+
+				<ImageSlide
+					url={
+						"https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+					}
+				/>
+
+				<Arrow
+					direction="right"
+					clickFunction={this.nextSlide}
+					glyph="&#9654;"
+				/>
 			</div>
 		);
 	}
