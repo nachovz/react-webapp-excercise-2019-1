59351df4151c5a11b67ab335516e70962673b41b
Daphness Fils-aime <sirdaph@gmail.com>
2019-02-20 04:25:49 +0000

    final

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -21,49 +21,62 @@ export class Navbar extends React.Component {
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
 					<ul className="navbar-nav">
-						<li className="nav-item active">
+						<li className="nav-item active fix">
 							<Link to="/" className="nav-link">
 								Home <span className="sr-only">(current)</span>
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className="nav-item fix">
 							<Link to="/blog" className="nav-link">
 								Blog
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className="nav-item fix">
 							<Link to="/checkout" className="nav-link">
 								Check Out
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className="nav-item fix">
 							<Link to="/demo" className="nav-link">
 								Demo
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className="nav-item fix">
 							<Link to="/single" className="nav-link">
 								Single
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className="nav-item fix">
 							<Link to="/login" className="nav-link">
 								Login
 							</Link>
 						</li>
-						<li className="nav-item">
+						<li className="nav-item fix">
 							<Link to="/store" className="nav-link">
 								StoreView
 							</Link>
 						</li>
 						<li className="nav-item space">
 							<Context.Consumer>
-								{({ store }) => {
+								{({ store, actions }) => {
 									{
 										return store.session.length != 0 ? (
-											<span>
-												{store.session[0].username}
-											</span>
+											<div className="row">
+												<span className="col-md-4 control">
+													{store.session[0].username}
+												</span>
+												<Link
+													to={"/login"}
+													className="nav-link ">
+													<span
+														className="col-md-4 adjust"
+														onClick={() =>
+															actions.logOut()
+														}
+													/>{" "}
+													log out
+												</Link>
+											</div>
 										) : (
 											<Link
 												to={"/login"}
